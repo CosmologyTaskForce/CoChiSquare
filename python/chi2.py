50d3601a470ba9771202ae8662b56795da7497dd
emptymalei <emptymalei@yahoo.cn>
2012-06-25 00:55:12 +0800

    Using only mathematica's own func.
    Complete the method using only mathematica's function.

--- python/chi2.py
+++ python/chi2.py
@@ -1,29 +1,46 @@
 # A program for chi2 fitting.
 # Find minimum of chi2. Using internal numpy and scipy.
 
-import numpy
+import numpy as np
+import scipy
 from scipy.integrate import quad
+import math as m
 
-# from scipy.optmize import fmin
 
-
+# Parameters
+.
 H0 = 71
 c  = 299792
 
-def Hubble(Omegam0,zt,z):
-    return H0 * sqrt( Omegam0*(1+z)^2 + 1/2 * Omegam0 * (1+zt)^3 +(1 - Omegam0 - 1/2 * Omegam0 * (1 + zt)^3 ) * (1 + z)^2 )
 
+# Define hubble function.
+
+def Hubble(Omegam0,zt,z):
+    return H0 * m.sqrt( Omegam0*(1+z)**2 + 1/2 * Omegam0 * (1+zt)**3 +(1 - Omegam0 - 1/2 * Omegam0 * (1 + zt)**3 ) * (1 + z)**2 )
+
+
+# Define 
 
 def Distancez(Omegam0,zt,z):
-    return c * (1+z) * quad(1/Hubble(Omegam0,zt,tmp),0,z)[0]
+    om0 = Omegam0
+    ztrans = zt
+    return c * (1+z) * quad(lambda tmp: 1/Hubble(om0,ztrans,tmp),0,z)
 
-Distancez(0.27,0.5,1)
+flag1 = Distancez(0.27,0.5,1)
+#print flag1 
 
 
 
 
 
+def testfunc(testpar,t):
+    tmp=testpar
+    return quad(lambda x: x+tmp, 1, t)
 
+print testfunc(7,4)
+
+test = quad(lambda x: x, 1, 3)
+print test
 
 
 
